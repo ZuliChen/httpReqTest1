e6a1a95c9ba0dcca371be1c6366aaaa89fdbb90f
Chen Zuli <resistance1008@gmail.com>
2023-12-12 10:07:12 +0800

    test

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const http = require("http");
+////node.js module http
 
-const server = http.createServer((req, res) => {
-  res.write("welcome homepage!!!");
-  res.end();
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.write("welcome homepage!!!");
+//   res.end();
+// });
+
+// server.listen(5566, () => {
+//   console.log("server is runnong on port 5566!!!");
+// });
+
+// express.js
+
+const express = require("express");
+const app = express();
+
+app.get("/firstpage", (req, res) => {
+  res.send("歡迎來到首頁");
 });
 
-server.listen(5566, () => {
-  console.log("server is runnong on port 5566!!!");
+app.get("/secondpage", (req, res) => {
+  res.send("歡迎來到第二頁");
+});
+
+app.listen(5566, () => {
+  console.log("伺服器正在聆聽port 5566......");
 });
