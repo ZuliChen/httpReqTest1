c0b225bccc9dc7fedfdec2828fc8560169043483
Chen Zuli <resistance1008@gmail.com>
2023-12-13 17:03:01 +0800

    Practice EJS

--- app.js
+++ app.js
@@ -16,47 +16,65 @@
 const express = require("express");
 const app = express();
 
+//how to use static file
+app.use(express.static("public"));
+//use EJS
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.get("/firstpage", (req, res) => {
-  res.send("歡迎來到首頁");
-});
-
-app.get("/secondpage", (req, res) => {
-  res.send("歡迎來到第二頁");
-});
-
-app.get("/sendFile", (req, res) => {
-  res.sendFile(__dirname + "/sendFile.html");
-});
-
-app.get("/json", (req, res) => {
-  let obj = {
-    englishName: "Zuli Chen",
-    browser: "Safari",
-  };
-  res.json(obj);
-});
-
-app.get("/redirect", (req, res) => {
-  res.redirect("/secondpage");
-});
-
-app.get("/fruit", (req, res) => {
-  res.send("welcome to  fruit page");
-});
-
-app.get("/fruit/:anyFriut", (req, res) => {
-  res.send("welcome to " + req.params.anyFriut + " page");
+  res.render("index");
 });
 
 app.get("/formHandling", (req, res) => {
-  res.send(
-    `已收到表單，使用者名稱為${req.query.username}，年齡為${req.query.age}`
-  );
+  let { username, age } = req.query;
+  res.render("response", { username, age });
 });
+
+// app.get("/:fruit", (req, res) => {
+//   res.render("index", { fruit: req.params.fruit });
+// });
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// app.get("/firstpage", (req, res) => {
+//   res.send("歡迎來到首頁");
+// });
+
+// app.get("/secondpage", (req, res) => {
+//   res.send("歡迎來到第二頁");
+// });
+
+// app.get("/sendFile", (req, res) => {
+//   res.sendFile(__dirname + "/sendFile.html");
+// });
+
+// app.get("/json", (req, res) => {
+//   let obj = {
+//     englishName: "Zuli Chen",
+//     browser: "Safari",
+//   };
+//   res.json(obj);
+// });
+
+// app.get("/redirect", (req, res) => {
+//   res.redirect("/secondpage");
+// });
+
+// app.get("/fruit", (req, res) => {
+//   res.send("welcome to  fruit page");
+// });
+
+// app.get("/fruit/:anyFriut", (req, res) => {
+//   res.send("welcome to " + req.params.anyFriut + " page");
+// });
+
+// app.get("/formHandling", (req, res) => {
+//   res.send(
+//     `已收到表單，使用者名稱為${req.query.username}，年齡為${req.query.age}`
+//   );
+// });
 
 app.get("*", (req, res) => {
   res.send("<h1>404 not found</h1>");
